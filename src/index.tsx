cf425f50a5cd812df80884d7196938cc355070cb
hh1718 <ajdah.233041@gmail.com>
2020-07-16 22:54:44 +0900

    firebase analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,21 @@ import './index.scss';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIDLJ4L6rm3WTArvgammxDVxv7vj_SjEY",
+  authDomain: "my-anytime-fitness-ranking.firebaseapp.com",
+  databaseURL: "https://my-anytime-fitness-ranking.firebaseio.com",
+  projectId: "my-anytime-fitness-ranking",
+  storageBucket: "my-anytime-fitness-ranking.appspot.com",
+  messagingSenderId: "898012485964",
+  appId: "1:898012485964:web:ead0ea2c14b7be6a4397e9",
+  measurementId: "G-SJ5K4EYHHH"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
